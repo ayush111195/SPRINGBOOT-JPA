5f3bb7b0096f1cc3bb8fab56a017345b5a499395
Ayush Gupta <121301901+ayush111195@users.noreply.github.com>
2023-08-27 00:21:52 +0530

    Update UserRespository.java

--- springbootjpa1/src/main/java/com/springboot/jpa/repo/UserRespository.java
+++ springbootjpa1/src/main/java/com/springboot/jpa/repo/UserRespository.java
@@ -2,7 +2,7 @@ package com.springboot.jpa.repo;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.springboot.jpa.entities.User;
+import com.springboot.jpa.entities. User;
 
 public interface UserRespository extends CrudRepository<User, Integer> {
 
